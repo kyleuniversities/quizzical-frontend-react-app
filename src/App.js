5f256e5ad336c18d94de011bec85da0bb479e4d9
kyleuniversities <kyleuniversities@gmail.com>
2023-12-02 06:45:18 -0800

    conformed site to router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './app/home/HomePage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
